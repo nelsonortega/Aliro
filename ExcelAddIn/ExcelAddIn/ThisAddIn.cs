0c7739bab1dc71f9ffafce872419a985178644e8
Cmirandaobregon <cmiranda@buklolab.com>
2018-08-30 15:55:41 -0600

    test

--- ExcelAddIn/ExcelAddIn/ThisAddIn.cs
+++ ExcelAddIn/ExcelAddIn/ThisAddIn.cs
@@ -13,7 +13,7 @@ namespace ExcelAddIn
     {
         private void ThisAddIn_Startup(object sender, System.EventArgs e)
         {
-            //coment
+            //coment 
         }
 
         private void ThisAddIn_Shutdown(object sender, System.EventArgs e)
